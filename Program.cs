7b326f05ecf0151c896f1e13de3fb504de6254d7
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-15 09:28:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provjera_znanja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite rečenicu: ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine(s1.Replace(" ","_"));
+            Console.ReadKey();
+        }
+    }
+}
